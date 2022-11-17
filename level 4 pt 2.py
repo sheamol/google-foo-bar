5a37fd31c63e1d9cc282374ce6d07ed21aee1ab8
Sheamol <obedasheamol@gmail.com>
2022-11-17 16:56:40 +0000

    complete lvl 4.2

--- level 4 pt 2.py
+++ level 4 pt 2.py
@@ -1,44 +1,39 @@
 import itertools
 
 
-def convert_to_path(perm):
-    perm = list(perm)
-    perm = [0] + perm + [-1]
-    path = list()
-    for i in range(1, len(perm)):
-        path.append((perm[i - 1], perm[i]))
-    return path
-
-
 def solution(time, time_limit):
+    """For a given time matrix, find max number of nodes to visit
+    Uses a brute force method, permuting through all the available bunnies, taking one less each time
+    until time constraint is met"""
+
     rows = len(time)
     bunnies = rows - 2
 
-    print('\n'.join(map(str, time)))
-    print('')
-
+    # deal with negative times i.e. increase in timer amount
     for i in range(rows):
         for ii in range(rows):
             for iii in range(rows):
                 time[ii][iii] = min(time[ii][iii], time[ii][i] + time[i][iii])
 
-    print('\n'.join(map(str, time)))
-    print('')
-
+    # if any diagonals are < 0 then you can create a path going to yourself thus giving infinite time,
+    # thus all bunnies saved
     for r in range(rows):
         if time[r][r] < 0:
-            return [i for i in range(bunnies)]
+            return range(bunnies)
 
-    for i in reversed(range(bunnies + 1)):
+    # permute through all possible bunny paths, starting at 0 ending at -1 (the end)
+    for i in reversed(range(bunnies + 1)):  # reverse used to start with all bunnies and work backwards
         for perm in itertools.permutations(range(1, bunnies + 1), i):
             total_time = 0
-            path = convert_to_path(perm)
-            for start, end in path:
-                total_time += time[start][end]
+            path = [0] + list(perm) + [-1]  # create path for bunny collection, start at 0, end at -1
+            for step in range(len(path) - 1):
+                total_time += time[path[step]][path[step + 1]]  # get time taken to get from current to next from
+                # matrix
+
             if total_time <= time_limit:
-                return sorted(list(i - 1 for i in perm))
+                return sorted(list(i - 1 for i in perm))    # -1 needed as bunny 0 is in row 1
     return None
 
 
 print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
-# print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
+print(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3))
