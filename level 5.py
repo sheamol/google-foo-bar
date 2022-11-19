35969e2ee1b8419746479a745c8c1d10246e8ebf
sheamol <obedasheamol@gmail.com>
2022-11-19 19:44:22 +0000

    complete level 5

--- level 5.py
+++ level 5.py
@@ -1,41 +1,27 @@
-from decimal import Decimal, localcontext
-import math
+"""Solution to find a sum of Beatty Sequence with alpha of sqrt 2."""
+from decimal import Decimal, getcontext
+
+# Set decimal precision globally and declare global constants needed for a Beatty sequence
+getcontext().prec = 102
+R = Decimal(2).sqrt()
+S = Decimal(2) + Decimal(2).sqrt()  # s = r/(r-1) which in this case is 2 + sqrt(2)
 
 
-def _solution(s):
-    a = Decimal(s)
-    with localcontext() as ctx:
-        ctx.prec = 102
-        r = Decimal(2).sqrt()
-        s = r / (r - 1)
-
-        def solve(k):
-            if k == 0:
-                return 0
-            n = int(r * k)
-            n_prime = int(Decimal(n) / s)
-            return (n * (n + 1)) / 2 - solve(n_prime) - n_prime * (n_prime + 1)
-
-        return str(int(solve(a)))
+def _solve(k):
+    """Recursive function to find summation using Raleigh-Beatty Theorem."""
+    if k == 0:
+        return 0
+    n = int(R * k)
+    n_prime = int(Decimal(n) / S)
+    return n * (n + 1) / 2 - n_prime * (n_prime + 1) - _solve(n_prime)
 
 
 def solution(s):
+    """Take position of stopping position for the summation, convert to decimal, then returnS summation as string."""
     a = Decimal(s)
-    with localcontext() as ctx:
-        ctx.prec = 102
-
-        def solve(n):
-            n = Decimal(n)
-            if n == 0:
-                return 0
-            n_prime = Decimal(math.floor((Decimal(2).sqrt() - Decimal(1)) * Decimal(n)))
-
-            return n * n_prime + n * (n + Decimal(1))/2 - n_prime * (n_prime + Decimal(1))/Decimal(2) - solve(n_prime)
-
-        return str(int(solve(a)))
+    return str(_solve(a))
 
 
 print(solution("77"))
 print(solution("5"))
 print(solution(str(10e100)))
-print(_solution(str(10e100)))
