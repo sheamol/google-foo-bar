21a2d1793784b0c7cdfba95b7bd0bb85088be51d
sheamol <obedasheamol@gmail.com>
2022-11-19 21:01:55 +0000

    decrypt final message

---
+++ final decoder/final.py
@@ -0,0 +1,13 @@
+import base64
+from itertools import cycle
+
+message = """
+FEUWEQIQDRYSSk9WT0UCFgQSHEJNTUgPAA4JAQAUHQBGTVVMSAcWEAQWBQAFSkNMSAcDAg4BHBZG
+TVVMSAsLBxMWDAwDAQpLQ0JCBQIbAQAXCAIJARZCRFtTTxAPAQAPBAcBQ01TTxcADw0FGxFCRFtT TxYACwpLQ0JCAg4cT0VbTUgbBgxEQxw=
+"""
+
+key = bytes("obedasheamol", "utf-8")
+code = bytes(a ^ b for a, b in zip(base64.b64decode(message), cycle(key)))
+res = eval(code.decode("utf-8"))
+
+[print(key, ':', value) for key, value in res.items()]
